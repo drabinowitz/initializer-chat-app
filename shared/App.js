81a757611e9d406b91bc473935725ed69ceb7429
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-26 16:10:23 -0500

    Remove jshint ignore and add return.

--- shared/App.js
+++ shared/App.js
@@ -6,14 +6,12 @@ var Link = Router.Link;
 var App = React.createClass({
   render: function () {
     return (
-      /*jshint ignore: start*/
       <div>
         <h1>Isomorphic Chat App Built With React Router Initializer</h1>
         <br />
         <Link to='home'>Home</Link>
         <RouteHandler />
       </div>
-      /*jshint ignore: end*/
     );
   }
 });
--- shared/Components/Html.js
+++ shared/Components/Html.js
@@ -2,19 +2,19 @@ var React = require('react');
 
 var Html = React.createClass({
   render: function () {
-    /*jshint ignore:start*/
-    <html>
-      <head>
-        <meta charSet="utf-8" />
-        <title>{this.props.title}</title>
-      </head>
-      <body>
-        <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
-      </body>
-      <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
-      <script src="/public/bundle.js" defer></script>
-    </html>
-    /*jshint ignore:end*/
+    return (
+      <html>
+        <head>
+          <meta charSet="utf-8" />
+          <title>{this.props.title}</title>
+        </head>
+        <body>
+          <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
+        </body>
+        <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
+        <script src="/public/bundle.js" defer></script>
+      </html>
+    );
   }
 });
 
