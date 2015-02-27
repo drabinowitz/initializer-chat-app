a1e0be5e754deca2f8d4ff9fd40d33c57863fafd
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-27 18:03:50 -0500

    Implement delete and update handling for Room.

--- shared/Components/Rooms/Room.js
+++ shared/Components/Rooms/Room.js
@@ -1,14 +1,61 @@
 var React = require('react');
 var Link = require('react-router').Link;
+var ReusableForm = require('../ReusableForm');
 
 var Room = React.createClass({
   propTypes: {
-    room: React.PropTypes.object.isRequired
+    room: React.PropTypes.object.isRequired,
+    handleUpdate: React.PropTypes.func.isRequired,
+    handleDelete: React.PropTypes.func.isRequired
+  },
+
+  getInitialState: function () {
+    return {
+      editing: false
+    };
+  },
+
+  showEdit: function () {
+    this.setState({
+      editing: true
+    });
+  },
+
+  onUpdate: function (newTitle) {
+    this.setState({
+      editing: false
+    });
+
+    var newRoom = {
+      id: this.props.room.id,
+      title: newTitle
+    };
+
+    this.props.handleUpdate(this.props.room, newRoom);
+  },
+
+  onDelete: function () {
+    this.handleDelete(this.props.room);
   },
 
   render: function () {
+    var toShow;
+    if (this.state.editing) {
+      toShow = (
+        <ReusableForm handleSubmit={this.onUpdate} defaultValue={this.props.room.title} />
+      );
+    } else {
+      toShow = (
+        <Link to='messages' params={{roomId: this.props.room.id}}>{this.props.room.title}</Link>
+      );
+    }
+
     return (
-      <Link to='messages' params={{roomId: this.props.room.id}}>{this.props.room.title}</Link>
+      <div>
+        {toShow}
+        <button onClick={this.showEdit}>Edit</button>
+        <button onClick={this.onDelete}>Delete</button>
+      </div>
     );
   }
 });
