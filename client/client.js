1f9b0e89358c54c3a278817fe8cad852df7d8ffa
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-27 11:40:50 -0500

    Reorder to require Routes last.

--- client/client.js
+++ client/client.js
@@ -3,7 +3,6 @@ var Router = require('react-router');
 var Fetcher = require('fetchr/libs/fetcher.client');
 
 var App = require('../shared/App');
-var Routes = require('../shared/Components/Routes');
 
 fetcher = new Fetcher({
   xhrPath: App.xhrPath,
@@ -15,6 +14,8 @@ App.fetcher = require('bluebird').promisifyAll(fetcher);
 [require('../shared/Stores/roomStore'), require('../shared/Stores/messageStore'), require('../shared/Stores/likeStore')].forEach(function (store) {
   store.rehydrate(window.state);
 });
+
+var Routes = require('../shared/Components/Routes');
 
 Router.run(Routes, Router.HistoryLocation, function (Handler) {
   React.render(<Handler />, window.document.getElementById('app'));
