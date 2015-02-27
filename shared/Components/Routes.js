326bd90e34de114335bec7867e66f66e204f76c3
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-27 11:24:25 -0500

    Rename App component to Home.

--- shared/Components/Routes.js
+++ shared/Components/Routes.js
@@ -1,12 +1,12 @@
 var Router = require('react-router');
 var Route = Router.Route;
 
-var App = require('./App');
+var Home = require('./Home');
 var RoomsOwner = require('./Rooms/RoomsOwner');
 var MessagesOwner = require('./Messages/MessagesOwner');
 
 var Routes = (
-  <Route name='home' path='/' handler={App}>
+  <Route name='home' path='/' handler={Home}>
     <Route name='rooms' path='/rooms' handler={RoomsOwner}>
       <Route name='messages' path='/rooms/:roomId' handler={MessagesOwner} />
     </Route>
