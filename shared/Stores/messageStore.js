ae2780d26378ddbfdac3ccecd9563b44fc38709a
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-27 10:02:11 -0500

    Basic messageStore implementation.

---
+++ shared/Stores/messageStore.js
@@ -0,0 +1,40 @@
+var Store = require('./Store');
+
+var appDispatcher = require('../Dispatcher/appDispatcher');
+var messageConstants = require('../Constants/messageConstants');
+
+var messageStore = new Store('message');
+
+messageStore.getAllForRoomId = function (roomId) {
+  var result = {};
+  for (var id in this._data) {
+    if (id === roomId.toString()) {
+      result[id] = this._data[id];
+    }
+  }
+  return result;
+};
+
+appDispatcher.register(function (payload) {
+  var type = payload.action.type;
+  var body = payload.action.body;
+  switch (type) {
+    case messageConstants.CREATE_MESSAGE:
+      messageStore.create(body.message);
+      break;
+
+    case messageConstants.READ_MESSAGE:
+      messageStore.read(body.messages);
+      break;
+
+    case messageConstants.UPDATE_MESSAGE:
+      messageStore.update(body.message);
+      break;
+
+    case messageConstants.DELETE_MESSAGE:
+      messageStore.delete(body.message);
+      break;
+  }
+});
+
+module.exports = messageStore;
