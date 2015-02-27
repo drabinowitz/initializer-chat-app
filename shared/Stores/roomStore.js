17ae3c6b77e5a16fcbafe1640c5ee80932797d33
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-27 10:03:20 -0500

    Basic roomStore implementation.

---
+++ shared/Stores/roomStore.js
@@ -0,0 +1,30 @@
+var Store = require('./Store');
+
+var appDispatcher = require('../Dispatcher/appDispatcher');
+var roomConstants = require('../Constants/roomConstants');
+
+var roomStore = new Store('room');
+
+appDispatcher.register(function (payload) {
+  var type = payload.action.type;
+  var body = payload.action.body;
+  switch (type) {
+    case roomConstants.CREATE_ROOM:
+      roomStore.create(body.room);
+      break;
+
+    case roomConstants.READ_ROOM:
+      roomStore.read(body.rooms);
+      break;
+
+    case roomConstants.UPDATE_ROOM:
+      roomStore.update(body.room);
+      break;
+
+    case roomConstants.DELETE_ROOM:
+      roomStore.delete(body.room);
+      break;
+  }
+});
+
+module.exports = roomStore;
