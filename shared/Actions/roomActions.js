06b4123ba3ed72d2a3c2e45b7dd0fbb73f84b8dc
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-27 11:55:26 -0500

    Create roomActions.

---
+++ shared/Actions/roomActions.js
@@ -0,0 +1,51 @@
+var appDispatcher = require('../Dispatcher/appDispatcher');
+var roomConstants = require('../Constants/roomConstants');
+var fetcher = require('../App').fetcher;
+var initializer = require('react-router-initializer');
+
+module.exports = {
+  create: function (room) {
+    fetcher.createAsync('room', {room: room}).then(function (room) {
+      appDispatcher.dispatchViewAction({
+        type: roomConstants.CREATE_ROOM,
+        body: {
+          room: room
+        }
+      });
+    });
+  },
+  read: function () {
+    var fetchRoom = fetcher.readAsync('room').then(function (rooms) {
+      appDispatcher.dispatchViewAction({
+        type: roomConstants.READ_ROOM,
+        body: {
+          rooms: rooms
+        }
+      });
+    });
+
+    this.fetchRoom = fetchRoom;
+    initializer.register(fetchRoom);
+  },
+  update: function (room) {
+    fetcher.updateAsync('room').then(function (newRoom) {
+      appDispatcher.dispatchViewAction({
+        type: roomConstants.UPDATE_ROOM,
+        body: {
+          room: room,
+          newRoom: newRoom
+        }
+      });
+    });
+  },
+  delete: function (room) {
+    fetcher.deleteAsync('room').then(function (room) {
+      appDispatcher.dispatchViewAction({
+        type: roomConstants.DELETE_ROOM,
+        body: {
+          room: room
+        }
+      });
+    });
+  }
+};
